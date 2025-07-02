347df61940307a9c74501980a47184ebac85d4a8
u-yamii <morimrua0130@icloud.com>
2025-07-03 08:06:55 +0900

    Initial commit of Strapi project for Render deployment

---
+++ src/api/park/routes/park.ts
@@ -0,0 +1,7 @@
+/**
+ * park router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::park.park');
---
+++ src/api/parks-hazard-level/routes/parks-hazard-level.ts
@@ -0,0 +1,7 @@
+/**
+ * parks-hazard-level router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::parks-hazard-level.parks-hazard-level');
